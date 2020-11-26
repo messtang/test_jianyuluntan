529b419878e495f222215881d33f97146fbe100e
张帅帅 <1327879272@qq.com>
2020-11-26 10:06:07 +0800

    Update ZSS_DataProvider.java

--- src/com/webtest/zssdemo/ZSS_DataProvider.java
+++ src/com/webtest/zssdemo/ZSS_DataProvider.java
@@ -9,32 +9,32 @@ import com.webtest.dataprovider.ExcelDataProvider;
 public class ZSS_DataProvider {
 	@DataProvider(name = "pictureFile")
 	public Object[][] pictureFile() throws IOException {
-		return new ExcelDataProvider().getTestDataByExcel("F:/test/testExcel/Demo.xlsx", "图片路径");
+		return new ExcelDataProvider().getTestDataByExcel("information_data.xlsx", "图片路径");
 	}
 
 	@DataProvider(name = "nickName")
 	public Object[][] nickName() throws IOException {
-		return new ExcelDataProvider().getTestDataByExcel("F:/test/testExcel/Demo.xlsx", "昵称");
+		return new ExcelDataProvider().getTestDataByExcel("information_data.xlsx", "昵称");
 	}
 
 	@DataProvider(name = "data")
 	public Object[][] data() throws IOException {
-		return new ExcelDataProvider().getTestDataByExcel("F:/test/testExcel/Demo.xlsx", "数据");
+		return new ExcelDataProvider().getTestDataByExcel("information_data.xlsx", "数据");
 	}
 
 	@DataProvider(name = "birthday")
 	public Object[][] birthday() throws IOException {
-		return new ExcelDataProvider().getTestDataByExcel("F:/test/testExcel/Demo.xlsx", "生日");
+		return new ExcelDataProvider().getTestDataByExcel("information_data.xlsx", "生日");
 	}
 
 	@DataProvider(name = "message")
 	public Object[][] message() throws IOException {
-		return new ExcelDataProvider().getTestDataByExcel("F:/test/testExcel/Demo.xlsx", "个性签名");
+		return new ExcelDataProvider().getTestDataByExcel("information_data.xlsx", "个性签名");
 	}
 
 	@DataProvider(name = "password")
 	public Object[][] password() throws IOException {
-		return new ExcelDataProvider().getTestDataByExcel("F:/test/testExcel/Demo.xlsx", "密码");
+		return new ExcelDataProvider().getTestDataByExcel("information_data.xlsx", "密码");
 	}
 
 }
