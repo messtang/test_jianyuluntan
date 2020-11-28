09247d0d038e871e35972a76c7e126937d5fa5f1
张帅帅 <1327879272@qq.com>
2020-11-28 15:05:10 +0800

    Update SignInTest.java

--- src/com/webtest/zssdemo/SignInTest.java
+++ src/com/webtest/zssdemo/SignInTest.java
@@ -5,6 +5,7 @@ import static org.testng.Assert.assertTrue;
 import java.io.IOException;
 
 import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 import com.webtest.core.BaseTest;
