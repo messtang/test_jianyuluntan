a8c5afbc539b467e4e18c9f0fe2394879c5d0c3d
liangchengyibei <45160558+liangchengyibei@users.noreply.github.com>
2020-12-07 11:18:16 +0800

    Update XDataProvider.java

--- src/com/webtest/xldemo/XDataProvider.java
+++ src/com/webtest/xldemo/XDataProvider.java
@@ -5,13 +5,16 @@ import java.io.IOException;
 import org.testng.annotations.DataProvider;
 
 import com.webtest.dataprovider.*;
+//作者：徐良
 public class XDataProvider {
 
+	//登录参数的数据提供者
 	@DataProvider(name = "loginData")
 	public Object[][] getDataOfLogin() throws IOException {
 		return new ExcelDataProvider()
 				.getTestDataByExcel("data.xlsx","login");
 	}
+	//注册参数的数据提供者
 	@DataProvider(name = "registerData")
 	public Object[][] getDataOfRegister() throws IOException {
 		return new ExcelDataProvider()
