c8550965a99d8dc00b3b49570a62457dd9a4b9f1
liangchengyibei <45160558+liangchengyibei@users.noreply.github.com>
2020-12-07 11:16:51 +0800

    Update TestDemo.java

--- src/com/webtest/xldemo/TestDemo.java
+++ src/com/webtest/xldemo/TestDemo.java
@@ -8,9 +8,10 @@ import java.io.IOException;
 import org.testng.annotations.Test;
 
 import com.webtest.core.BaseTest;
-
+//作者：徐良
 public class TestDemo extends BaseTest{
 
+	//测试登录的方法
 	@Test(dataProviderClass=XDataProvider.class,dataProvider = "loginData")
 	private void testLogin(String username,String password) throws InterruptedException, IOException {
 		webtest.open("/index.php/denglu.html");
@@ -22,7 +23,7 @@ public class TestDemo extends BaseTest{
         assertTrue(webtest.isTextPresent("剑鱼论坛后台"));
         webtest.click("xpath=/html/body/nav/div[1]/ul/li[4]");
 	}
-	
+	//测试注册的方法
 	@Test(dataProviderClass=XDataProvider.class,dataProvider ="registerData")
 	public void testRegister(String username,String password,String again,String mail) throws InterruptedException {
 		webtest.open("/index.php/zhuce.html");
@@ -37,6 +38,7 @@ public class TestDemo extends BaseTest{
         assertTrue(webtest.isTextPresent("登陆成功"));
         webtest.click("xpath=/html/body/nav/div[1]/ul/li[4]");
 	}
+	//测试签到状态的方法
 	@Test
 	public void testQiandao() throws InterruptedException {
 		webtest.open("/index.php/index.html");
@@ -44,7 +46,7 @@ public class TestDemo extends BaseTest{
 		webtest.click("id=qiandao");
 		assertTrue(webtest.isTextPresent("已签到"));
 	}
-	
+	//测试发帖的方法
 	@Test
 	public void testFatie() throws InterruptedException {
 		webtest.open("/index.php/index.html");
@@ -52,7 +54,7 @@ public class TestDemo extends BaseTest{
 		webtest.click("xpath=/html/body/div[1]/div/div[2]/div[1]/div/div[2]/a");
 		assertTrue(webtest.isTextPresent("剑鱼论坛后台"));
 	}
-	
+	//测试搜索框的方法
 	@Test
 	public void testFind() throws InterruptedException {
 		webtest.open("/index.php/index.html");
